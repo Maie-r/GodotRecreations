02b586e6afdd954a5a8dee906e1b3fa635ef4f5d
Maie- <nitainandi1@gmail.com>
2025-08-28 23:50:16 -0300

    Preset folders added for editing on mobile

---
+++ Celeste/scripts/Berry.cs
@@ -0,0 +1,7 @@
+using System;
+using Godot;
+
+public partial class Berry : Area2D
+{
+    
+}
---
+++ Celeste/scripts/Madeline.cs
@@ -0,0 +1,7 @@
+using System;
+using Godot;
+
+public partial class Madeline : CharacterBody2D
+{
+    
+}
