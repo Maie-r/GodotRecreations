88ffe7944ea24ca838f7fc43e1c746c543b5a95b
Nitai Nandi <49562104+Maie-r@users.noreply.github.com>
2025-09-04 20:34:51 -0300

    Atualizar o Trampoline.cs

--- Celeste/scripts/Trampoline.cs
+++ Celeste/scripts/Trampoline.cs
@@ -3,5 +3,14 @@ using Godot;
 
 public partial class Trampoline : Area2D
 {
+    [Export]
+    public float PushForce = 100;
+
+    public delegate OnJumpedEventHandler;
     
+    private void _on_body_entered(Node2D body)
+    {
+       if (body is IPushable b)
+          b.ApplyImpulse(this.GetNormal() * PushForce)
+    }
 }
