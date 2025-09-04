9f6018322c635f05f9bdc6831fcdf9ca19943c45
Nitai Nandi <49562104+Maie-r@users.noreply.github.com>
2025-09-04 20:47:45 -0300

    Atualizar o Madeline.cs

--- Celeste/scripts/Madeline.cs
+++ Celeste/scripts/Madeline.cs
@@ -3,5 +3,43 @@ using Godot;
 
 public partial class Madeline : CharacterBody2D
 {
-    
+    [Export]
+    public float JumpForce = 50;
+
+    [Export]
+    public float BaseSpeed = 200;
+
+    public int MaxJumpCount = 1;
+    public int JumpCount = 1;
+
+    public int MaxDashCount = 1;
+    public int DashCount = 1;
+
+    public GroundedSafetyTimer;
+
+
+    public override void _Ready()
+    {
+        SetTimers();
+    }
+
+    public override void _Process(double delta)
+    {
+
+    }
+}
+
+// SETUPS
+public partial class Madeline : CharacterBody2D
+{
+    SetTimers()
+    {
+        GroundedSafetyTimer = new Timer()
+        {
+            Oneshot = true,
+            WaitTime = 3
+        };
+
+        AddChild(GroundedSafetyTimer);
+    }
 }
