0848e610a09552e8b3c6d458074ee9a5e6a54149
Nitai Nandi <49562104+Maie-r@users.noreply.github.com>
2025-09-04 20:40:14 -0300

    Atualizar o Berry.cs

--- Celeste/scripts/Berry.cs
+++ Celeste/scripts/Berry.cs
@@ -3,5 +3,21 @@ using Godot;
 
 public partial class Berry : Area2D
 {
-    
+    private void _on_body_entered(Node2D body)
+    {
+        if (body is Madeline m)
+           BerryGot(m)
+        
+    }
+
+    public void BerryGot(Madeline m)
+    {
+        m.GroundSafetyTimer.Timeout += Collected;
+    }
+
+    public void Collected()
+    {
+        GD.Print("Berry Got!");
+        QueueFree();
+    }
 }
