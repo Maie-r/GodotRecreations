eb02b10755b6215346ca26bca48077f67b4560b6
Nitai Nandi <49562104+Maie-r@users.noreply.github.com>
2025-09-04 21:46:33 -0300

    Atualizar o Madeline.cs

--- Celeste/scripts/Madeline.cs
+++ Celeste/scripts/Madeline.cs
@@ -1,7 +1,7 @@
 using System;
 using Godot;
 
-public partial class Madeline : CharacterBody2D
+public partial class Madeline : CharacterBody2D, IPushable
 {
     [Export]
     public float JumpForce = 50;
@@ -14,8 +14,14 @@ public partial class Madeline : CharacterBody2D
 
     public int MaxDashCount = 1;
     public int DashCount = 1;
+    public Timer DashCooldownTimer;
+    public bool CanDash = true;
 
-    public GroundedSafetyTimer;
+    public Timer GroundedSafetyTimer;
+
+    public Timer CoyoteTimer;
+
+    public float Gravity = 50;
 
 
     public override void _Ready()
@@ -25,11 +31,37 @@ public partial class Madeline : CharacterBody2D
 
     public override void _Process(double delta)
     {
+       Vector2 vel = Velocity;
+       vel.Y += Gravity * (float)delta;
+
        /*
        if (OnGround)
           SetState(States.Grounded)
-       
+       else
+       {
+          if (JumpCount == MaxJumpCount)
+             CoyoteTimer.Start();
+          SetState(States.Falling);
+       }
+
+       var Axis = Input.GetVector("left","right","up","down");
+       if (Axis.X == Vector.Zero)
+       {
+          vel.X *= 0.9f;
+       }
+       else 
+       {
+          vel.X += BaseSpeed * Axis.X * delta;
+       }
        */
+
+       if (Input.ActionJustPressed("dash"))
+          ApplyImpulse(Axis * 200);
+    }
+
+    public override void ApplyImpulse(Vector2 impulse) 
+    {
+       Velocity += impulse;
     }
 }
 
@@ -54,12 +86,29 @@ public partial class Madeline : CharacterBody2D
 {
     SetTimers()
     {
+        DashCooldownTimer = new Timer()
+        {
+            Oneshot = true,
+            WaitTime = 1,
+            TimeOut += () => CanDash = true;
+        };
         GroundedSafetyTimer = new Timer()
         {
             Oneshot = true,
             WaitTime = 3
         };
+        CoyoteTimer = new Timer(){
+            Oneshot = true,
+            WaitTime = 1,
+            TimeOut += () => JumpCount = 0
+        };
 
         AddChild(GroundedSafetyTimer);
+        AddChild(CoyoteTimer);
     }
+}
+
+public interface IPushable
+{
+    public void ApplyImpulse(Vector2 impulse);
 }
